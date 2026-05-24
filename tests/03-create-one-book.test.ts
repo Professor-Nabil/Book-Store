5745d8218f8a6d3f2f77981aeeeb87a9fef8b269
Nabil Tester <tester@example.com>
2026-05-24 08:24:21 +0100

    test: should success create new book

---
+++ tests/03-create-one-book.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import request from "supertest";
+import app from "../src/app.js";
+import { faker } from "@faker-js/faker";
+
+describe("### API ### POST '/api/books'", () => {
+  it("Should success create new book", async () => {
+    const res = await request(app).post("/api/books").send({
+      author: faker.book.author(),
+      title: faker.book.title(),
+    });
+
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("message");
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data).toHaveProperty("id");
+    expect(res.body.data).toHaveProperty("author");
+    expect(res.body.data).toHaveProperty("title");
+  });
+});
